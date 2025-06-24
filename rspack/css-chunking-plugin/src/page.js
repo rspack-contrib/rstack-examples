9a825c45d955987788dd48ef1c921b4126056f93
Cong-Cong <dacongsama@live.com>
2025-06-24 17:48:25 +0800

    feat: css chunking plugin demo

---
+++ rspack/css-chunking-plugin/src/page.js
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+import "./styles/theme.css";
+import "./styles/component.css";
+
+document.addEventListener
+const button = document.createElement('button');
+button.className = 'btn';
+button.style.width = '200px';
+button.style.height = '50px';
+button.textContent = 'background should be black';
+document.body.appendChild(button);
