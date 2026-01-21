546a8d77b63174c6789463f088df89c045341bed
Max <fi3ework@gmail.com>
2026-01-21 19:43:47 +0800

    chore(rstest): bump Rstest to 0.8.0 (#399)

--- rstest/browser-rsbuild-vanilla/rstest.config.ts
+++ rstest/browser-rsbuild-vanilla/rstest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@rstest/core';
 export default defineConfig({
   browser: {
     enabled: true,
+    provider: 'playwright',
     browser: 'chromium',
     port: 3010,
   },
